de5820e61715e1d71c483ee6e76fd7e6fecb0699
xanish <danishali7866@gmail.com>
2020-06-15 13:43:22 +0530

    Implemented book details page

--- local-library/controllers/book.js
+++ local-library/controllers/book.js
@@ -46,7 +46,37 @@ exports.book_list = (req, res, next) => {
 
 // Display detail page for a specific book.
 exports.book_detail = (req, res) => {
-    res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
+        
+    async.parallel({
+
+        book: function (callback) {
+
+            Book.findById(req.params.id)
+                .populate('author')
+                .populate('genre')
+                .exec(callback);
+
+        },
+
+        book_instance: function (callback) {
+
+            BookInstance.find({ book: req.params.id })
+                .exec(callback);
+
+        }
+
+    }, (err, results) => {
+        if (err) { return next(err); }
+
+        if (results.book == null) {
+            var err = new Error('Book not found');
+            err.status = 404;
+            return next(err);
+        }
+
+        res.render('book_detail', { title: results.book.title, book: results.book, book_instances: results.book_instance });
+    });
+
 };
 
 // Display book create form on GET.
