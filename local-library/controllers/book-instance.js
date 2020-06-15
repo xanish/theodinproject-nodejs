e6f7679f8473abd778eb1dcad89366131fab20f2
xanish <danishali7866@gmail.com>
2020-06-15 14:03:11 +0530

    Implemented book instance detail page

--- local-library/controllers/book-instance.js
+++ local-library/controllers/book-instance.js
@@ -14,8 +14,22 @@ exports.bookinstance_list = (req, res, next) => {
 };
 
 // Display detail page for a specific BookInstance.
-exports.bookinstance_detail = (req, res) => {
-    res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+exports.bookinstance_detail = (req, res, next) => {
+    
+    BookInstance.findById(req.params.id)
+        .populate('book')
+        .exec(function (err, bookInstance) {
+            if (err) { return next(err); }
+
+            if (bookInstance == null) {
+                var err = new Error('Book copy not found');
+                err.status = 404;
+                return next(err);
+            }
+
+            res.render('bookinstance_detail', { title: 'Copy: ' + bookInstance.book.title, bookinstance: bookInstance });
+        });
+
 };
 
 // Display BookInstance create form on GET.
