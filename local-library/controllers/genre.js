41a6e5e389d42d0f3e793fb7ba3b3574d82186a8
xanish <danishali7866@gmail.com>
2020-06-16 16:18:52 +0530

    Installed express-validator and used it to implement create genre form

--- local-library/controllers/genre.js
+++ local-library/controllers/genre.js
@@ -2,6 +2,7 @@ var Book = require('../models/book');
 var Genre = require('../models/genre');
 
 var async = require('async');
+var validator = require('express-validator');
 
 // Display list of all Genre.
 exports.genre_list = (req, res, next) => {
@@ -51,13 +52,44 @@ exports.genre_detail = (req, res, next) => {
 
 // Display Genre create form on GET.
 exports.genre_create_get = (req, res) => {
-    res.send('NOT IMPLEMENTED: Genre create GET');
+    res.render('genre_form', { title: 'Create Genre' });
 };
 
 // Handle Genre create on POST.
-exports.genre_create_post = (req, res) => {
-    res.send('NOT IMPLEMENTED: Genre create POST');
-};
+exports.genre_create_post = [
+
+    validator.body('name', 'Genre name required').trim().isLength({ min: 1}),
+
+    validator.sanitizeBody('name'),escape(),
+
+    (req, res, next) => {
+
+        var errors = validator.validationResult(req);
+
+        var genre = new Genre({ name: req.body.name });
+
+        if (!errors.isEmpty()) {
+            res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array() });
+        }
+        else {
+            Genre.findOne({ name: req.body.name })
+                .exec(function (err, found_genre) {
+                    if (err) { return next(err); }
+
+                    if (found_genre) {
+                        res.redirect(found_genre.url);
+                    }
+                    else {
+                        genre.save(function (err) {
+                            if (err) { return next(err); }
+
+                            res.redirect(genre.url);
+                        });
+                    }
+                });
+        }
+    }
+];
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = (req, res) => {
