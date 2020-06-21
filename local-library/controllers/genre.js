9c2ec900a4193a74f0eaafd3026cb4538af7c3a3
xanish <danishali7866@gmail.com>
2020-06-21 16:05:24 +0530

    Implemented genre delete page

--- local-library/controllers/genre.js
+++ local-library/controllers/genre.js
@@ -90,13 +90,60 @@ exports.genre_create_post = [
 ];
 
 // Display Genre delete form on GET.
-exports.genre_delete_get = (req, res) => {
-    res.send('NOT IMPLEMENTED: Genre delete GET');
+exports.genre_delete_get = (req, res, next) => {
+    
+    async.parallel({
+
+        genre: function (callback) {
+            Genre.findById(req.params.id).exec(callback);
+        },
+
+        genre_books: function (callback) {
+            Book.find({ genre: req.params.id }).exec(callback);
+        }
+
+    }, (err, results) => {
+        if (err) { return next(err); }
+
+        if (results.genre==null) {
+            res.redirect('/catalog/genres');
+        }
+
+        res.render('genre_delete', { title: 'Delete Genre', genre: results.genre, genre_books: results.genre_books } );
+    });
+
 };
 
 // Handle Genre delete on POST.
-exports.genre_delete_post = (req, res) => {
-    res.send('NOT IMPLEMENTED: Genre delete POST');
+exports.genre_delete_post = (req, res, next) => {
+    
+    async.parallel({
+
+        genre: function (callback) {
+            Genre.findById(req.body.genreid).exec(callback);
+        },
+
+        genre_books: function (callback) {
+            Book.find({ genre: req.body.genreid }).exec(callback);
+        }
+
+    }, (err, results) => {
+        if (err) { return next(err); }
+
+        if (results.genre_books.length > 0) {
+            // Genre has books. Render in same way as for GET route.
+            res.render('genre_delete', { title: 'Delete Genre', genre: results.genre, genre_books: results.genre_books } );
+        }
+        else {
+            // Genre has no books. Delete object and redirect to the list of genre.
+            Genre.findByIdAndRemove(req.body.genreid, function deleteGenre(err) {
+                if (err) { return next(err); }
+                // Success - go to genre list
+                res.redirect('/catalog/genres')
+            });
+        }
+    });
+
 };
 
 // Display Genre update form on GET.
