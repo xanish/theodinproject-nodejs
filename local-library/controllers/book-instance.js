9e263f7f327764bf5160c8415f58bdb949f2efbd
xanish <danishali7866@gmail.com>
2020-06-22 11:39:14 +0530

    Implemented delete book instance form

--- local-library/controllers/book-instance.js
+++ local-library/controllers/book-instance.js
@@ -92,12 +92,30 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = (req, res) => {
-    res.send('NOT IMPLEMENTED: BookInstance delete GET');
+    
+    BookInstance.findById(req.params.id)
+        .populate('book')
+        .exec(function (err, bookinstance) {
+            if (err) { return next(err); }
+
+            if (bookinstance==null) {
+                res.redirect('/catalog/bookinstances');
+            }
+
+            res.render('bookinstance_delete', { title: 'Delete BookInstance', bookinstance:  bookinstance});
+        });
+
 };
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = (req, res) => {
-    res.send('NOT IMPLEMENTED: BookInstance delete POST');
+    
+    BookInstance.findByIdAndRemove(req.body.bookinstanceid, function deleteBookInstance(err) {
+        if (err) { return next(err); }
+
+        res.redirect('/catalog/bookinstances');
+    });
+
 };
 
 // Display BookInstance update form on GET.
